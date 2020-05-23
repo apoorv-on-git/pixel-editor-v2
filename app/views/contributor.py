b70f549ac06bbadb2e0ee049feaffaa79e77b815
Apoorv Goyal <apoorvelous@gmail.com>
2020-05-23 23:01:24 +0530

    basic flow corrected

--- app/views/contributor.py
+++ app/views/contributor.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect, url_for
 from app.utils.role_util import *
 from app.db_controllers.contributor_controller import *
 
@@ -6,6 +6,8 @@ contributor = Blueprint('contributor', __name__, url_prefix="/")
 
 @contributor.route('/')
 def login():
+    if session.get("document_id"):
+        return redirect(url_for("contributor.dashboard"))
     return render_template("/contributor/login/login.html")
 
 @contributor.route("/dashboard")
