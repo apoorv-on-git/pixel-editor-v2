d870c5b2c44103bac6f3b29001dcf3f516124a91
Apoorv Goyal <apoorvelous@gmail.com>
2020-07-13 01:06:29 +0530

    super admin done

--- app/views/super_admin.py
+++ app/views/super_admin.py
@@ -1,41 +1,39 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request, jsonify
 from app.utils.role_util import *
-from app.db_controllers.graphics_controller import *
+from app.db_controllers.super_admin_controller import *
 from app.db_controllers.helper import *
 from app.data.grade_breakdown import data as grade_breakdown
 from app.data.formula import formula_list
 
-graphics = Blueprint('graphics', __name__, url_prefix="/graphics")
+super_admin = Blueprint('super_admin', __name__, url_prefix="/super-admin")
 
-@graphics.route('/')
+@super_admin.route('/')
 def login():
-    if session.get("graphics_id"):
-        return redirect(url_for("graphics.dashboard"))
+    if session.get("super_admin_id"):
+        return redirect(url_for("super_admin.dashboard"))
     elif session.get("admin_id"):
         return redirect(url_for("admin.dashboard"))
     elif session.get("contributor_id"):
         return redirect(url_for("contributor.dashboard"))
-    elif session.get("super_admin_id"):
-        return redirect(url_for("super_admin.dashboard"))
-    return render_template("/graphics/login/login.html")
+    elif session.get("graphics_id"):
+        return redirect(url_for("graphics.dashboard"))
+    return render_template("/super_admin/login/login.html")
 
-@graphics.route("/dashboard")
-@required_role_as_graphics()
+@super_admin.route("/dashboard")
+@required_role_as_super_admin()
 def dashboard():
     session["question_id_list"] = None
-    user_data = get_user_document_data(session.get('graphics_id'))
-    sidebar_filter_based_on_requirement = firebase_get_questions_for_graphics()
-    return render_template( "/graphics/dashboard/dashboard.html",
+    user_data = get_user_document_data(session.get('super_admin_id'))
+    return render_template( "/super_admin/dashboard/dashboard.html",
                             grade_breakdown=grade_breakdown,
-                            user_data=user_data,
-                            sidebar_filter_based_on_requirement=sidebar_filter_based_on_requirement
+                            user_data=user_data
                         )
 
-@graphics.route("/levels")
-@required_role_as_graphics()
+@super_admin.route("/levels")
+@required_role_as_super_admin()
 def levels():
     session["question_id_list"] = None
-    user_data = get_user_document_data(session.get('graphics_id'))
+    user_data = get_user_document_data(session.get('super_admin_id'))
     try:
         grade = request.args.get("grade")
         grade_dict = next((grade_dict for grade_dict in grade_breakdown if grade_dict.get('grade_number') == int(grade)), None)
@@ -52,18 +50,16 @@ def levels():
                 "message": "Invalid Chapter!"
             }), 400
         levels = chapter_dict.get("levels")
-        sidebar_filter_based_on_requirement = firebase_get_questions_for_graphics()
-        questions_for_graphics = sidebar_filter_based_on_requirement.get(f"NCERT_G{int(grade):02}_TOPIC{int(chapter):02}")
+        questions_for_super_admin = firebase_get_approved_question_count(f"NCERT_G{int(grade):02}_TOPIC{int(chapter):02}")
         for level in levels:
             level_id = f"NCERT_G{int(grade):02}_TOPIC{int(chapter):02}_LEVEL{int(level.get('level_number')):02}"
-            level["questions_for_graphics"] = questions_for_graphics.get(level_id) or 0
-        return render_template( "/graphics/level/level.html",
+            level["questions_for_super_admin"] = questions_for_super_admin.get(level_id) or 0
+        return render_template( "/super_admin/level/level.html",
                                 levels=levels,
                                 chapter_name=chapter_dict.get("chapter_name"),
                                 grade=grade,
                                 chapter=chapter,
-                                grade_breakdown=grade_breakdown,
-                                sidebar_filter_based_on_requirement=sidebar_filter_based_on_requirement
+                                grade_breakdown=grade_breakdown
                             )
     except ValueError:
         return jsonify({
@@ -71,11 +67,10 @@ def levels():
             "message": "Do not tamper with the URL. Go to dashboard and try again!"
         }), 400
 
-@graphics.route("edit-question")
-@required_role_as_graphics()
-def edit_question():
-    user_data = get_user_document_data(session.get('graphics_id'))
-    sidebar_filter_based_on_requirement = firebase_get_questions_for_graphics()
+@super_admin.route("/review-question")
+@required_role_as_super_admin()
+def review_question():
+    user_data = get_user_document_data(session.get('super_admin_id'))
     try:
         grade = int(request.args.get("grade"))
         chapter = int(request.args.get("chapter"))
@@ -87,16 +82,16 @@ def edit_question():
                 "message": "Do not tamper with the URL. Go to dashboard and try again!"
             })
         if not document_id:
-            question_document_ids = firebase_get_questions_for_graphics_list(grade, chapter, level)
+            question_document_ids = firebase_get_questions_for_super_admin_list(grade, chapter, level)
             if len(question_document_ids) == 0:
                 return jsonify({
                     "status": "error",
-                    "message": "No questions for graphics!"
+                    "message": "No questions for super admin!"
                 }), 400
             session["question_id_list"] = question_document_ids
         question_document_list = session.get("question_id_list")
         if not question_document_list:
-            return redirect(url_for("graphics.dashboard"))
+            return redirect(url_for("super_admin.dashboard"))
         elif len(question_document_list) == 0:
             return jsonify({
                 "status": "error",
@@ -109,8 +104,9 @@ def edit_question():
         except IndexError:
             next_document_id = ""
         question_data = firebase_get_question(document_id, grade, chapter, level)
+        admin_name = question_data.get("approved_by")
         grade_dict, chapter_dict, level_dict = get_grade_breakdown_dict(grade, chapter, level)
-        return render_template( "/graphics/question/edit_question.html",
+        return render_template( "/super_admin/question/review_question.html",
                                 grade=grade,
                                 chapter=chapter,
                                 level=level,
@@ -119,7 +115,8 @@ def edit_question():
                                 next_document_id=next_document_id,
                                 document_id=document_id,
                                 question_data=question_data,
-                                sidebar_filter_based_on_requirement=sidebar_filter_based_on_requirement
+                                pdf_url=chapter_dict.get("chapter_pdf"),
+                                admin_name=admin_name
                             )
     except ValueError:
         return jsonify({
